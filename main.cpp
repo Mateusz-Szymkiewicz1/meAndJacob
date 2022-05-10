6ae51cc24d44b86af93a9f9673acc8978ea51745
at20zgorzelskij <103038756+at20zgorzelskij@users.noreply.github.com>
2022-05-10 14:12:28 +0200

    una de la costa fiesta ceeseso come la fa

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include "QFile"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    //QFile stylesheet(":/qss/stylesheet.qss");
+    //stylesheet.open(QIODevice::ReadOnly | QIODevice::Text);
+    //a.setStyleSheet(stylesheet.readAll());
     w.show();
     return a.exec();
 }
