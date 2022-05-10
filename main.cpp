a240e8cd1033aef57b4d15fa538955b43c67b06b
at20zgorzelskij <103038756+at20zgorzelskij@users.noreply.github.com>
2022-05-10 14:30:14 +0200

    ceesesito

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    QFile stylesheet(":/qss/stylesheet.qss");
+    QFile stylesheet(":/qrc/stylesheet.qss");
     stylesheet.open(QIODevice::ReadOnly | QIODevice::Text);
     a.setStyleSheet(stylesheet.readAll());
     w.show();
